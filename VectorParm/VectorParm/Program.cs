fa75b7bda290b23b36da9342cb3da35125a6a146
Estudiante PUCESE <estudiante@pucese.edu.ec>
2018-12-13 08:57:48 -0500

    Ordenar Vectores

--- VectorParm/VectorParm/Program.cs
+++ VectorParm/VectorParm/Program.cs
@@ -12,7 +12,13 @@ namespace VectorParm
         static void Main(string[] args)
         {
             int[] x = new int[MAX];
+            string[] x = new string[MAX];
             leer(x);
+            imprimir(x);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Ordenar(x);
+            Console.WriteLine("Vector ordenar");
             imprimir(x);
             if (buscar(x, 15) == true)
                 Console.WriteLine("\n15 Encontrado");
@@ -22,17 +28,42 @@ namespace VectorParm
                 Console.WriteLine("\n15 Encontrado");
             else
                 Console.WriteLine("\n15 no existe");
+            //trabajoando con el vector de caracteres
+            Console.WriteLine("\nIngrese los Nombres");
+            leerNombres(nombres);
+            imprimirNombres(nombres);
+
             Console.ReadKey();
 
+        }
+        public static void Ordenar(int[]x)
+        {
+            int aux = 0;
+            for (int i = 0; i< x.Length;i++)
+                for(int j=i+1;j < x.Length;j++)
+                   if (x[i]>x[j])
+                   { //ingresar el elemento del vector
+                        aux = x[i];
+                        x[i] = x[j];
+                        x[j] = aux;
+                   }
+                
+            
         }
         public static void leer(int[] x)
         {
             for (int i=0;i < MAX;i++)
             {
                 Console.WriteLine("\nIngresa el elemento {0}", i + 1);
-                x[i] = Int32.Parse(Console.ReadLine());
-
-                    
+                x[i] = Int32.Parse(Console.ReadLine());  
+            }
+        }
+        public static void leerNombres(string[] x)
+        {
+            for (int i = 0; i < MAX; i++)
+            {
+                Console.WriteLine("\nIngresa el Nombre {0}", i + 1);
+                x[i] = (Console.ReadLine());
             }
         }
         public static void imprimir(int[] x)
@@ -40,6 +71,13 @@ namespace VectorParm
             foreach(int num in x)
             {
                 Console.WriteLine("\nElemento {0}", num);
+            }
+        }
+        public static void imprimirNombres(string[] x)
+        {
+            foreach (string num in x)
+            {
+                Console.WriteLine("\nNombre {0}", num);
             }
         }
         public static bool buscar(int[] x, int n )
